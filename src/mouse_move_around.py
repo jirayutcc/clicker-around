4b45e3ab1a4df6a77ed6cf19be97501cad3dbb6e
jirayut cc <jirayut_ssww@hotmail.com>
2024-06-07 13:59:11 +0700

    update: mouseMoveAround

--- src/mouse_move_around.py
+++ src/mouse_move_around.py
@@ -6,6 +6,8 @@ def mouseMoveAround():
     while True: 
         x = random.randint(600, 700)
         y = random.randint(200, 600)
+        
+        print('mouse move!')
         pag.moveTo(x, y, 0.5)
-        time.sleep(240)
+        time.sleep(180)
         # 300(5 Min), 1800(30 Min)
